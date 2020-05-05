5a19a64d3580a9ae6880a1dfb41bdc7028bdf44c
ChungZH <124694738@qq.com>
2020-05-05 11:42:54 +0800

    feat: add more info to aboutwindow

--- src/ui/aboutwindow.cpp
+++ src/ui/aboutwindow.cpp
@@ -3,12 +3,34 @@
 
 #include <QApplication>
 #include <QAction>
+#include <QFile>
+#include <QDebug>
+#include <QTextStream>
 
 AboutWindow::AboutWindow(QWidget *parent)
       : QDialog(parent), ui(new Ui::AboutWindow)
 {
   ui->setupUi(this);
   setWindowTitle("About Notepanda");
+
+  QFile ver(":/asset/makespec/VERSION");
+  QFile verSuf(":/asset/makespec/VERSIONSUFFIX");
+  QFile buildVer(":/asset/makespec/BUILDVERSION");
+  ver.open(QFile::ReadOnly);
+  verSuf.open(QFile::ReadOnly);
+  buildVer.open(QFile::ReadOnly);
+
+  QTextStream t_ver(&ver);
+  QTextStream t_vSf(&verSuf);
+  QTextStream t_bVe(&buildVer);
+
+  QString verString = t_ver.readAll() + t_vSf.readAll() + " BV" + t_bVe.readAll();
+
+  ui->label_5->setText(verString);
+
+  ver.close();
+  verSuf.close();
+  buildVer.close();
 
   a_AboutQt = new QAction(tr("About Qt"), this);
   ui->AboutQt->setDefaultAction(a_AboutQt);
