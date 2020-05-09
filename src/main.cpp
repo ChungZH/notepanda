e456c1080f8368a4fd7424b48eb3ee10ff71934d
ChungZH <chungzh07@gmail.com>
2020-05-09 16:44:57 +0800

    feat: open from command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <QApplication>
-#include <QStyleFactory>
+#include <QCommandLineParser>
 #include <QDebug>
+#include <QStyleFactory>
 
 #include "./ui/mainwindow.h"
 
 int main(int argc, char *argv[])
 {
   QApplication App(argc, argv);
+  QApplication::setStyle(QStyleFactory::create("Fusion"));
   App.setOrganizationName("ChungZH");
   App.setApplicationName("Notepanda");
   App.setApplicationVersion("0.0.1-rc.1");
 
-  QApplication::setStyle(QStyleFactory::create("Fusion"));
+  QCommandLineParser parser;
+  parser.addHelpOption();
+  parser.addPositionalArgument("source", "The source file to open.");
+  parser.process(App);
+
   MainWindow notepanda;
   notepanda.show();
+  if (parser.positionalArguments().size() == 1)
+    notepanda.plainTextEdit->openFile(parser.positionalArguments().at(0));
   return App.exec();
 }
